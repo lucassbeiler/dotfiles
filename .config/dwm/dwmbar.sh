fc13db0340d2687dd3eddb9afdb90a8c0b0061bd
Lucas S. Beiler <lucasbeiler@protonmail.com>
2019-09-01 13:45:08 -0300

    [dwmbar.sh] fix logic and indentation

--- .config/dwm/dwmbar.sh
+++ .config/dwm/dwmbar.sh
@@ -24,16 +24,16 @@ Bateria() {
 	BATERIA=$(cat /sys/class/power_supply/BAT1/capacity)
 	if [ $(cat /sys/class/power_supply/ADP1/online) -eq '1' ]; then
 		iconeBat="\uf5e7" #carregando
-  elif [ "$BATERIA" -le '75' ]; then
-		iconeBat="\uf241" #3/4 de 100%
-  elif [ "$BATERIA" -le '50' ]; then
-    iconeBat="\uf242" #metade
-  elif [ "$BATERIA" -le '25' ]; then
-    iconeBat="\uf243" #1/4 de 100%
-  elif [ "$BATERIA" -le '15' ]; then
-    iconeBat="\uf244" #fim
-	else
-		iconeBat="\uf240" #76%~100%
+	elif [ "$BATERIA" -gt '75' ]; then
+		iconeBat="\uf240" #76% ~ 100% 
+	elif [[ "$BATERIA" -le '75' ]] && [[ "$BATERIA" -gt '50' ]]; then
+	 	iconeBat="\uf241" #3/4 de 100%
+	elif [[ "$BATERIA" -le '50' ]] && [[ "$BATERIA" -gt '25' ]]; then
+    		iconeBat="\uf242" #metade
+	elif [[ "$BATERIA" -le '25' ]] && [[ "$BATERIA" -gt '15' ]]; then
+    		iconeBat="\uf243" #1/4 de 100%
+	elif [ "$BATERIA" -le '15' ]; then
+    		iconeBat="\uf244" #fim
 	fi
 	echo -en "$iconeBat $BATERIA%"
 }
@@ -44,18 +44,20 @@ Rede() {
 }
 
 Volume() {
-  volume=$(amixer get Master | grep -o -E [[:digit:]]+% | tail -n1)
+	volume=$(amixer get Master | grep -o -E [[:digit:]]+% | tail -n1)
 	volumeInt=$(amixer get Master | grep -o -E [[:digit:]]+% | tail -n1 | cut -d "%" -f1)
-  if [ "$volumeInt" -le '48' ]; then
-    iconeVol="\uf027"
-	elif [ "$volumeInt" -le '5' ]; then
-    iconeVol="\uf026"
-	elif [ "$volumeInt" -eq '0' ]; then
-    iconeVol="\uf6a9"
-  else
-    iconeVol="\uf028"
-  fi
-  echo -en "$iconeVol $volume"
+
+	if [ "$volumeInt" -gt '48' ]; then
+		iconeVol="\uf028"
+	elif [[ "$volumeInt" -le '48' ]] && [[ "$volumeInt" -gt '5' ]]; then
+    		iconeVol="\uf027"
+	elif [[ "$volumeInt" -le '5' ]] && [[ "$volumeInt" -gt '0' ]]; then
+    		iconeVol="\uf026"
+	else
+    		iconeVol="\uf6a9"
+  	fi
+
+  	echo -en "$iconeVol $volume"
 }
 
 Proce(){
