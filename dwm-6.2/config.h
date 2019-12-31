a76e67f12dcea99f5898bbb26aa7c984bb457727
Lucas S. Beiler <lucasbeiler@protonmail.com>
2019-12-31 06:39:38 -0300

    [dwm6.2/config.h] Improved Firejail arguments

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -78,10 +78,9 @@ static Key keys[] = {
 	{ 0,				XF86XK_AudioMute,        spawn, SHCMD("maim -u -s | xclip -selection clipboard -t image/png") },
 
 	/* standard bindings */
-	{ MODKEY|ShiftMask,		XK_c,	   spawn,	   SHCMD("firejail --private=prv-chromim --disable-mnt chromium --incognito --start-maximized") },
-	/*{ MODKEY|ShiftMask,		XK_c,	   spawn,	   SHCMD("firejail --private=prv-chromim --disable-mnt --noroot --apparmor firefox-bin --private-window") }, */
-	{ MODKEY|ShiftMask,		XK_m,	   spawn,	   SHCMD("firejail --private=prv-deezer --disable-mnt chromium --start-maximized") },
-	{ MODKEY|ShiftMask,		XK_t,	   spawn,	   SHCMD("firejail --private=prv-telego --seccomp --disable-mnt --noroot --apparmor telegram-desktop") },
+	{ MODKEY|ShiftMask,		XK_c,	   spawn,	   SHCMD("firejail --private=prv-chromim chromium --incognito --start-maximized") },
+	{ MODKEY|ShiftMask,		XK_m,	   spawn,	   SHCMD("firejail --private=prv-deezer chromium --start-maximized --app=https://deezer.com/es/loved") },
+	{ MODKEY|ShiftMask,		XK_t,	   spawn,	   SHCMD("firejail --private=prv-telego telegram-desktop") },
 
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
